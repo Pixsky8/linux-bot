5390617e9b91a1e72e5c085f35c2ae91f4c62aee
Pixsky8 <pixsky8@gmail.com>
2020-05-24 19:07:11 +0200

    .

--- src/command.py
+++ src/command.py
@@ -9,7 +9,7 @@ current_dir = "/data"
 max_message = 2
 
 async def on_command(message, client, config):
-    markdown = "Markdown"
+    markdown = ""
     reply = ""
     return_code = 0
     content = message.content[config.prefix_len:]
@@ -22,7 +22,11 @@ async def on_command(message, client, config):
         if config.is_admin(message.author.id):
             command = command_arg("", content.split()[1:], "")
             proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-            (out, err) = proc.communicate()
+            try:
+                (out, err) = proc.communicate(timeout=40)
+            except subprocess.TimeoutExpired:
+                proc.kill()
+                (out, err) = proc.communicate()
             return_code = proc.returncode
             reply = out.decode("utf-8")
         else:
@@ -56,7 +60,7 @@ async def on_command(message, client, config):
             proc = subprocess.Popen("su quarantedeux -c \"" + content + '\"', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
             try:
                 (out, err) = proc.communicate(timeout=40)
-            except TimeoutExpired:
+            except subprocess.TimeoutExpired:
                 proc.kill()
                 (out, err) = proc.communicate()
             return_code = proc.returncode
@@ -64,7 +68,7 @@ async def on_command(message, client, config):
             proc = subprocess.Popen(content, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
             try:
                 (out, err) = proc.communicate(timeout=40)
-            except TimeoutExpired:
+            except subprocess.TimeoutExpired:
                 proc.kill()
                 (out, err) = proc.communicate()
             return_code = proc.returncode
@@ -83,7 +87,7 @@ def command_arg(command, args, suffix = None):
     """
     res = command
     for arg in args:
-        res = res + arg
+        res = res + ' ' + arg
     if suffix:
         res = res + suffix
     return res
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -i -t --memory="512m" --memory-swap="4m" --pids-limit 20 -v $(pwd)/data:/data linux-bot
+docker run -i -t --memory="1024m" --memory-swap="0m" --pids-limit 20 -v $(pwd)/data:/data linux-bot
